2b8a4b2445c7afa9f75fb17e7d97dc0d7a155748
glassbrickstudio <55618307+glassbrickstudio@users.noreply.github.com>
2019-11-24 04:55:40 +0530

    Game Settings and singleton video 4
    https://youtu.be/_HsYXclGm7k

---
+++ Photon Networking 2/Assets/Scripts/TestConnect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("connecting to server");
+
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+       
+
+
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to master");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason" + cause.ToString());
+
+
+    }
+
+
+
+
+
+}
